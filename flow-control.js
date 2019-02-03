e9feb841167745e68b2c01111553bea90ff3af93
Bryant Kim <bryantkkim@gmail.com>
2019-02-03 16:40:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,3 +1,10 @@
+var basicTeenager = "13"
+var basicTeenager = "14"
+var basicTeenager = "15"
+var basicTeenager = "16"
+var basicTeenager = "17"
+var basicTeenager = "18"
+var basicTeenager = "19"
 function basicTeenager(age) {
   if (age > 12 && age < 20) {
     console.log('You are a teenager!')
